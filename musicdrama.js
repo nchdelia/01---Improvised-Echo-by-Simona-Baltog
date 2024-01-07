b2f8de948b0a72a1f576cae3bf3d8dd8172ac6d8
nchdelia <nechita.delia9@gmail.com>
2024-01-07 21:50:23 +0200

    Courses pages

--- musicdrama.js
+++ musicdrama.js
@@ -36,34 +36,3 @@ const hiddenElements = document.querySelectorAll('.hidden-element');
 hiddenElements.forEach((element) => {
     observer.observe(element);
 });
-
-// animation on scroll settings - settings for prices section
-const pricesObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show-prices');
-        } else {
-            entry.target.classList.remove('show-prices');
-        }
-    });
-});
-
-const hiddenPrices = document.querySelectorAll('.hidden-prices');
-hiddenPrices.forEach((element) => {
-    pricesObserver.observe(element);
-});
-
-const trialPricesObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show-trial-prices');
-        } else {
-            entry.target.classList.remove('show-trial-prices');
-        }
-    });
-});
-
-const hiddenTrialPrices = document.querySelectorAll('.hidden-trial-prices');
-hiddenTrialPrices.forEach((element) => {
-    trialPricesObserver.observe(element);
-});
---
+++ singingclasses.js
@@ -0,0 +1,15 @@
+// animation on scroll settings
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden-element');
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+});
